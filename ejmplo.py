32e036e6f11a3692123fe371a43d9158af07a2d3
JhosuePani <paniaguita1019@gmail.com>
2023-02-04 14:02:56 -0500

    update clase

--- ejmplo.py
+++ ejmplo.py
@@ -4,11 +4,15 @@ class Perro:
     def __init__(self, nombre, color, raza, peludo):
         self.nombre = nombre
         self.color = color
-        self.raza = raza
+        self.race = raza
         self.peludo = peludo
 
-    def comiendo( self, nombre ):
-        print( f"{nombre} esta comiendo" )
+    def comiendo( self ):
+        print( f"{self.nombre} esta comiendo" )
 
-    def raza( self, nombre, raza, peludo ):
-        print( f"{nombre} es de la raza: {raza} y es {peludo}" )
+    def Race( self ):
+        print( f"{self.nombre} es de la raza: {self.race} y {self.peludo} es peludo" )
+
+mi_perro = Perro( "Samuel", "negro", "Pastor aleman", "Si" )
+mi_perro.comiendo()
+mi_perro.Race()
