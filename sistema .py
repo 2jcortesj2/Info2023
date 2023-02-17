04cd85f8d932be01317f32c4fec84b37a8cbe59c
JhosuePani <paniaguita1019@gmai.com>
2023-02-17 11:28:42 -0500

    UPDATE

--- sistema .py
+++ sistema .py
@@ -17,13 +17,13 @@ class Person:
 
     ## GETTERS ##
     def getName( self ):
-        return print (self.__nombre)
+        return self.__nombre
 
     def getCedula( self ):
-        return print (self.__cedula)
+        return self.__cedula
 
     def getGenero( self ):
-        return print (self.__genero)
+        return self.__genero
     
     def guardarInfo( self ):
         return self.__nombre, self.__cedula, self.__genero
@@ -85,10 +85,14 @@ class Sistema( Person ):
     
     def __init__(self):
         self.__lista_pacientes = []
-        
+        self.__lista_nombre = []
+        self.__lista_cedula = []
+        self.__lista_genero = []
+        self.__diccionario_pacientes = {}
+
     def numeroDePacientes( self ):
         self.__numero_pacientes = len( self.__lista_pacientes )
-        return self.__numero_pacientes
+        return print( self.__numero_pacientes )
     
     def ingresarPaciente( self, rol ):
         p = Paciente() 
@@ -97,14 +101,45 @@ class Sistema( Person ):
         p.setCedula( rol )
         p.assignService()
         self.__lista_pacientes.append( p.guardarInfo() )
-        print( self.__lista_pacientes )
-        print( self.numeroDePacientes() )
-            
+        self.__lista_nombre.append( p.getName() )
+        self.__lista_cedula.append( p.getCedula() )
+        self.__lista_genero.append( p.getGenero() )
+        self.__diccionario_pacientes.update( {'Nombre' : self.__lista_nombre, 'Cedula' : self.__lista_cedula, 'Genero' : self.__lista_genero} )
+        print( self.__diccionario_pacientes )
+        #print( self.__lista_pacientes )
+        #print( self.numeroDePacientes() )
 
-"""
-Un ejemplo para correr el codigo seria el siguienteñ
-"""
+    def verDatosPacientesLista( self ):
+        cedula = input( 'Ingrese la cedula del paciente que quiere buscar en la lista: ' )
+        for c in self.__lista_pacientes:
+            if cedula == c[1]:
+                return print(c)
 
+    def verDatosPacientesDiccionario( self ):
+        cedula = input( "Ingresar la cedula del pacientes que busca en el diccionario: " )
+        for p, c in enumerate( self.__diccionario_pacientes['Cedula'] ):
+            if cedula == c:
+                print( f"Nombre: { self.__diccionario_pacientes['Nombre'][p] }, cedula : {self.__diccionario_pacientes['Cedula'][p]}, Genero: {self.__diccionario_pacientes['Genero'][p]}" )
+                
 
-paciente_1 = Sistema()
-paciente_1.ingresarPaciente( "Paciente" )
+##########################################################
+
+p = Sistema()
+
+while True:
+    opcion = int( input( """
+    1- Ingresar paciente
+    2- Ver datos del paciente
+    3- Ver el numero de pacientes
+    4- Salir
+    > """ ) )
+
+    if opcion == 1:
+        p.ingresarPaciente( "Paciente" )
+    elif opcion == 2:
+        p.verDatosPacientesDiccionario()
+    elif opcion == 3:
+        p.numeroDePacientes()
+    elif opcion == 4:
+        break
+
