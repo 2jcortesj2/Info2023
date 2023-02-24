7dc0bbf8cc7ca7a8f43df5afdbf3663cca742907
JhosuePani <paniaguita1019@gmail.com>
2023-02-24 12:03:29 -0500

    update

--- __pycache__/mongo.py
+++ __pycache__/mongo.py
@@ -3,10 +3,10 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://josuepaniagua:Ba5319466@cluster0.megrtoc.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 
-my_date_base = client["hbdd"]
+my_date_base = client["Primera"]
 my_collection = my_date_base["Clientes"]
 
-my_dictionary = { "nombre": "juan", "direccion": 0 } 
+my_dictionary = { "nombre": "camilo", "direccion": 0 } 
 
 x = my_collection.insert_one( my_dictionary )
 
@@ -15,24 +15,27 @@ print( x.inserted_id ) # lo que hace el inserted_id es mostrarme como el objeto 
 
 
 ### Para buscar en la base de datos se usa el find
-for x in my_collection.find( { "direccion": 0} ):
-    print( x )
-
-
+"""
+for i in my_collection.find( { "direccion": 0} ):
+    print( i )
+"""
+#print( my_collection.find( { "direccion": 0 } ) )
 
 # si quiero que me muestre todo en la base de datos 
+
 for y in my_collection.find():
     print(y)
+    my_collection.delete_one( y )
+    print( y )
+
+
+# ¿ que pasa si me equivoque asignando un valor?
+my_query = { "nombre": "camilo", "direccion": 0 }
+new_values = { "$set": { "nombre": "Luis" } }
+
+my_collection.update_one( my_query, new_values )
 
 
 
 ## Para eliminar uno
-my_collection.delete_one(  )
-
-
-
-# ¿ que pasa si me equivoque asignando un valor?
-myquery = { "nombre": "camilo", "direccion": 0 }
-new_values = { "$set": { "nombre": "Luis" } }
-
-my_collection.update_one( myquery, new_values )
+#my_collection.delete_one( "Objeto que se quiere eliminar" )
